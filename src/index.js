7449b93c2b6a0ff345c804af6c73520cbfad123c
marilynpi <marilyn.pirrello@gmail.com>
2022-05-31 18:29:02 +0000

    adding ssl to node

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const morgan = require('morgan'); // shows http methods in console
 const cors = require('cors'); // needed to enable cors
+const https = require('https');
+const fs = require('fs');
 
 const routes = require('./routes/tasks.routes');
 
 const app = express();
 const port = 4000;
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tierracristal.ar/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tierracristal.ar/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/tierracristal.ar/chain.pem', 'utf8');
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -18,6 +24,14 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(port);
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+const httpsServer = https.createServer(credentials, app);
 
+//app.listen(port);
+
+httpsServer.listen(port);
 console.log('Server on port ' + port );
